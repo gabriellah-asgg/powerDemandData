30f094c2a47b6261f758fc4bcac5350365e31a55
GabriellaHoover <gabriellahoover@smithgill.com>
2024-08-06 11:32:06 -0500

    added documentation

--- generateCombinedData.py
+++ generateCombinedData.py
@@ -16,51 +16,66 @@ def get_files(folder_filepath):
     return file_list, section_list
 
 
-def process_file(csv_filepath, sect, dataframe):
+def preprocess_dataframe(dataframe):
     """
-    Takes in filepath to given csv to process, the corresponding section title, and the dataframe to concat the data
-    to. Calculates sums and averages of specific column titles and adds them to the input dataframe with the
-    corresponding section title.
-
-    RETURNS: Dataframe with sums and averages of given section added.
+    Cleans dataframe by coercing non-numeric values to nan and then dropping rows with nan
+    :param dataframe: dataframe to be processed
+    :return: processed dataframe with only numeric values
     """
-    df = pd.read_csv(csv_filepath, skiprows=10)
-
-    # ensure inputs are numeric
-    df = df[['EArray', 'EOutInv', 'PR']]
-    for column in df.columns:
-        df[column] = pd.to_numeric(df[column], errors='coerce')
-    df.fillna(0, inplace=True)
-
-    # calc sums
-    earray_sum = np.nansum(df['EArray'])
-    eoutinv_sum = np.nansum(df['EOutInv'])
-
-    # calc avg
-    pr_avg = round(np.nansum(df['PR']) / len(df['PR']), 4)
-
-    result_df = pd.DataFrame(
-        {'Section': [sect], 'EArray Sum': [earray_sum], 'EOutInv Sum': [eoutinv_sum], 'PR Average': [pr_avg]})
-
-    dataframe = pd.concat([dataframe, result_df])
-
+    df_columns = ['EArray', 'EOutInv', 'PR']
+    for column in df_columns:
+        dataframe[column] = pd.to_numeric(dataframe[column], errors='coerce')
+    dataframe = dataframe.dropna()
     return dataframe
 
 
+def calc_sums(list_of_df, position):
+    """
+    Calculates sums and averages of data on the given row (position)
+    :param list_of_df: list of dataframes to perform calculations on
+    :param position: integer of which row to calculate
+    :return: list of rounded sums and averages to be used as a row in the dataframe to export
+    """
+    sum_earray = 0
+    sum_eoutinv = 0
+    sum_pr = 0
+
+    for df in list_of_df:
+        sum_earray += df.loc[position, 'EArray']
+        sum_eoutinv += df.loc[position, 'EOutInv']
+        sum_pr += df.loc[position, 'PR']
+
+    avg_pr = sum_pr / len(list_of_df)
+    row_sum = [sum_earray, sum_eoutinv, avg_pr]
+    row_sum = [round(num, 4) for num in row_sum]
+
+    return row_sum
+
+
+# get list of filepaths to process
 files, sections = get_files(filepath)
+files = [filepath + '\\' + f for f in files]
 
-# initialize dataframe to export
-column_names = ['Section', 'EArray Sum', 'EOutInv Sum', 'PR Average']
-export_dataframe = pd.DataFrame(columns=column_names)
+# collect list of dataframes and clean them
+list_of_dataframes = [pd.read_csv(f, skiprows=10) for f in files]
+list_of_dataframes = [preprocess_dataframe(df) for df in list_of_dataframes]
 
-# process data in each file
-for index, f in enumerate(files):
-    export_dataframe = process_file(filepath + '\\' + f, sections[index], export_dataframe)
+# copy first dataframe to use as template for one to export
+export_dataframe = list_of_dataframes[0].copy()
+
+# calculate sums and averages for each row and populate database to export with them
+for index, row in export_dataframe.iterrows():
+    new_row_vals = calc_sums(list_of_dataframes, index)
+    export_dataframe.loc[index, 'EArray'] = new_row_vals[0]
+    export_dataframe.loc[index, 'EOutInv'] = new_row_vals[1]
+    export_dataframe.loc[index, 'PR'] = new_row_vals[2]
+
+export_dataframe.rename(columns={'EArray': 'EArray Sum', 'EOutInv': 'EOutInv Sum', 'PR': 'PR Avg'}, inplace=True)
 
 # export the combined data
 output_filepath = r'Q:\Projects\224008\DESIGN\ANALYSIS\00_PV\Power Demand Graph' + '\\' + 'CombinedPowerData.csv'
 try:
-    export_dataframe.to_csv(output_filepath)
+    export_dataframe.to_csv(output_filepath, index=False)
     print("\nFile output to: \n" + output_filepath)
 except PermissionError:
     print("\nCannot write to file, try closing " + output_filepath + " and rerunning")
